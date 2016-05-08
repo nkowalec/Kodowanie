bccc4c9d5043ed42121fe40412d271a45f7ede24
Nikodem Kowalec <nkowalec@hotmail.com>
2016-05-08 14:30:22 +0200

    Dodanie projektu dla kodowania arytmetycznego

---
+++ ArythmeticCode/Arythmetic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArythmeticCode
+{
+    public class Schema
+    {
+        
+    }
+
+    public class SNode
+    {
+        public int Level { get; private set; }
+        
+    }
+}
