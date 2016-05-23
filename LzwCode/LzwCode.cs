8c038858d7b426c85dd4c098b90539a8c70e3767
Nikodem Kowalec <nkowalec@hotmail.com>
2016-05-23 17:37:39 +0200

    Implementacja kodowania LZW

--- LzwCode/LzwCode.cs
+++ LzwCode/LzwCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,82 @@ namespace LzwCode
 {
     public class LzwCode
     {
+        private Stream Stream { get; set; }
+
+        public LzwCode(Stream _stream)
+        {
+            Stream = _stream;
+        }
+
+        public List<int> Compress()
+        {
+            // build the dictionary
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add(((char)i).ToString(), i);
+
+            string w = string.Empty;
+            List<int> compressed = new List<int>();
+
+            using (StreamReader sr = new StreamReader(Stream)) {
+                foreach (char c in sr.ReadToEnd())
+                {
+                    string wc = w + c;
+                    if (dictionary.ContainsKey(wc))
+                    {
+                        w = wc;
+                    }
+                    else
+                    {
+                        // write w to output
+                        compressed.Add(dictionary[w]);
+                        // wc is a new sequence; add it to the dictionary
+                        dictionary.Add(wc, dictionary.Count);
+                        w = c.ToString();
+                    }
+                }
+            }
+            // write remaining output if necessary
+            if (!string.IsNullOrEmpty(w))
+                compressed.Add(dictionary[w]);
+
+            return compressed;
+        }
+
+        public string Decompress()
+        {
+            List<int> compressed;
+            using (StreamReader sr = new StreamReader(Stream))
+            {
+                compressed = new List<int>(sr.ReadToEnd().Cast<int>());
+            }
+
+            // build the dictionary
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add(i, ((char)i).ToString());
+
+            string w = dictionary[compressed[0]];
+            compressed.RemoveAt(0);
+            StringBuilder decompressed = new StringBuilder(w);
+
+            foreach (int k in compressed)
+            {
+                string entry = null;
+                if (dictionary.ContainsKey(k))
+                    entry = dictionary[k];
+                else if (k == dictionary.Count)
+                    entry = w + w[0];
+
+                decompressed.Append(entry);
+
+                // new sequence; add it to the dictionary
+                dictionary.Add(dictionary.Count, w + entry[0]);
+
+                w = entry;
+            }
+
+            return decompressed.ToString();
+        }
     }
 }
